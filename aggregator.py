da6fcf782bae65681673fb175f63e3134748d094
svchyk <sputnik.ras@gmail.com>
2026-03-14 01:34:31 +0300

    Update aggregator.py

--- aggregator.py
+++ aggregator.py
@@ -8,7 +8,7 @@ import re
 # ==========================================
 # СПИСОК КАНАЛОВ И НАСТРОЙКИ
 # ==========================================
-CHANNELS = ['chirpnews', 'condottieros', 'infantmilitario']
+CHANNELS = ['chirpnews', 'condottieros', 'infantmilitario', 'victorstepanych']
 ARCHIVE_FILE = 'archive.json'
 
 def get_tg_posts(channel_name):
